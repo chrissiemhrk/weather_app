02a01d38b0e7024a89c0e9fc4b344753d8efa10f
chrissiemhrk <chrissiemhrk@gmail.com>
2022-06-14 11:01:31 +0200

    add weather forecast api

--- lib/services/weather_api_client.dart
+++ lib/services/weather_api_client.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import 'package:weather_app/models/weather.dart';
+import 'package:weather_app/models/current_weather.dart';
+import 'package:weather_app/models/weather_forecast.dart';
 
-class WeatherApiClient {
+class CurrentWeatherApi {
   static const baseUrl = 'http://api.weatherapi.com/v1';
   String? apiKey = dotenv.env['WEATHER_API_KEY'];
 
@@ -15,5 +16,20 @@ class WeatherApiClient {
     }
     var body = jsonDecode(weatherResponse.body);
     return Weather.fromJson(body);
+  }
+}
+
+class WeatherForecastApi {
+  static const baseUrl = 'http://api.weatherapi.com/v1';
+  String? apiKey = dotenv.env['WEATHER_API_KEY'];
+
+  Future<WeatherForecast> fetchWeatherForecast(String city) async {
+    final url = Uri.parse('$baseUrl/forecast.json?key=$apiKey&q=$city&days=4');
+    final weatherResponse = await http.get(url);
+    if (weatherResponse.statusCode != 200) {
+      throw Exception('error getting weather for $city');
+    }
+    var body = jsonDecode(weatherResponse.body);
+    return WeatherForecast.fromJson(body);
   }
 }
